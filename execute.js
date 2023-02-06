10b56bb36aa146cef4231403d542dc953ca2fe17
Alin Coop <alincoop@gmail.com>
2023-02-06 17:03:08 +1100

    setTimeout() added
    Delay execution to allow full pageload

--- execute.js
+++ execute.js
@@ -1,5 +1,5 @@
-// this code will be executed when the extension's button is clicked
-(function() {
+// this code will be executed when clicking on extension icon
+setTimeout(function() {
   document.querySelectorAll('[href*="shorts"]').forEach(function(elem){elem.parentElement.parentElement.parentElement.style.display='none'});
   console.log('execute.js executed');
-})();
+},1000);
--- pageload.js
+++ pageload.js
@@ -1,5 +1,5 @@
 // this code will be executed after page load
-(function() {
+setTimeout(function() {
   document.querySelectorAll('[href*="shorts"]').forEach(function(elem){elem.parentElement.parentElement.parentElement.style.display='none'});
   console.log('pageload.js executed');
-})();
+},1000);
